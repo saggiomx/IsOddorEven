97f5efebfe69791f2500a3afcdf80ee26ecd5830
Otman Ledezma <Otman.Ledezma@hexagon.com>
2021-12-28 09:31:23 -0600

    Add unit testing

--- src/Saggiomx.IsOddorEven.Api.Main.Tests/Controllers/OddorEvenControllerTests.cs
+++ src/Saggiomx.IsOddorEven.Api.Main.Tests/Controllers/OddorEvenControllerTests.cs
@@ -15,8 +15,7 @@ namespace Saggiomx.IsOddorEven.Api.Main.Tests.Controllers
         private Mock<ILogger<OddOrEvenController>> _mockLogger;
         private Mock<INumberValidator> _validator;
         private Mock<IIsOddOrEvenService> _service;
-
-        private OddOrEvenController controller;
+        private OddOrEvenController _oddOrEvenController;
 
         public OddorEvenControllerTests()
         {
@@ -26,23 +25,101 @@ namespace Saggiomx.IsOddorEven.Api.Main.Tests.Controllers
         }
 
         [Fact]
+        public void OddOrEvenController_Constructor_ArgumentNullException_Validator()
+        {
+            Assert.Throws<ArgumentNullException>(() => new OddOrEvenController(null, _service.Object, _mockLogger.Object));
+        }
+
+        [Fact]
+        public void OddOrEvenController_Constructor_ArgumentNullException_Service()
+        {
+            Assert.Throws<ArgumentNullException>(() => new OddOrEvenController(_validator.Object, null, _mockLogger.Object));
+        }
+
+        [Fact]
+        public void OddOrEvenController_Constructor_ArgumentNullException_Logger()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new OddOrEvenController(_validator.Object, _service.Object, null));
+        }
+
+        [Fact]
+        public void OddOrEvenController_Constructor_Success()
+        {
+            _oddOrEvenController = new OddOrEvenController(_validator.Object, _service.Object, _mockLogger.Object);
+
+            Assert.NotNull(_oddOrEvenController);
+            Assert.IsType<OddOrEvenController>(_oddOrEvenController);
+        }
+
+        [Fact]
         public void OddOrEvenController_IsEven()
         {
-            //arrange
             _validator.Setup(v => v.IsValid(It.IsAny<string>())).Returns(true);
             _service.Setup(s => s.HasReminder(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
 
-            controller = new OddOrEvenController(_validator.Object, _service.Object, _mockLogger.Object);
+            _oddOrEvenController = new OddOrEvenController(_validator.Object, _service.Object, _mockLogger.Object);
 
-            var result = controller.IsEven(It.IsAny<string>());
+            var result = _oddOrEvenController.IsEven(It.IsAny<string>());
 
             var okResult = result as OkObjectResult;
             Assert.NotNull(okResult);
             Assert.Equal(200, okResult.StatusCode);
-            Assert.Equal(false,okResult.Value);
+            Assert.Equal(false, okResult.Value);
 
-            _validator.Verify(v=> v.IsValid(It.IsAny<string>()), Times.Once);
-            _service.Verify(s => s.HasReminder(It.IsAny<int>(), It.IsAny<int>()), Times.Once);
+            _validator.Verify(v => v.IsValid(It.IsAny<string>()), Times.Once());
+            _service.Verify(s => s.HasReminder(It.IsAny<int>(), It.IsAny<int>()), Times.Once());
+        }
+
+        [Fact]
+        public void OddOrEvenController_400Response()
+        {
+            _validator.Setup(v => v.IsValid(It.IsAny<string>())).Returns(false);
+            _service.Setup(s => s.HasReminder(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+
+            _oddOrEvenController = new OddOrEvenController(_validator.Object, _service.Object, _mockLogger.Object);
+
+            var result = _oddOrEvenController.IsEven(It.IsAny<string>());
+
+            var errorResult = result as BadRequestObjectResult;
+            Assert.NotNull(errorResult);
+            Assert.Equal(400, errorResult.StatusCode);
+            Assert.IsType<ErrorResponse>(errorResult.Value);
+
+            _validator.Verify(v => v.IsValid(It.IsAny<string>()), Times.Once());
+            _service.Verify(s => s.HasReminder(It.IsAny<int>(), It.IsAny<int>()), Times.Never);
+        }
+
+        [Fact]
+        public void OddOrEvenController_IsOdd()
+        {
+            _validator.Setup(v => v.IsValid(It.IsAny<string>())).Returns(true);
+            _service.Setup(s => s.HasReminder(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+
+            _oddOrEvenController = new OddOrEvenController(_validator.Object, _service.Object, _mockLogger.Object);
+
+            var result = _oddOrEvenController.IsOdd(It.IsAny<string>());
+
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(true, okResult.Value);
+        }
+
+        [Fact]
+        public void OddOrEvenController_IsOddOrEven_Ok()
+        {
+            _validator.Setup(v => v.IsValid(It.IsAny<string>())).Returns(true);
+            _service.Setup(s => s.HasReminder(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+
+            _oddOrEvenController = new OddOrEvenController(_validator.Object, _service.Object, _mockLogger.Object);
+
+            var result = _oddOrEvenController.IsEven(It.IsAny<string>());
+
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(false, okResult.Value);
         }
 
     }
--- src/Saggiomx.IsOddorEven.Api.Main.Tests/Service/IsOddOrEvenServiceTests.cs
+++ src/Saggiomx.IsOddorEven.Api.Main.Tests/Service/IsOddOrEvenServiceTests.cs
@@ -25,16 +25,13 @@ namespace Saggiomx.IsOddorEven.Api.Main.Tests.Service
             Assert.Equal(expectedResult,result);
         }
 
-        [Theory]
-        [InlineData(2, 0)]
-        public void IsOddOrEvenService_HasRemider_DividedByZeroException(int dividend, int divisor)
+        [Fact]
+        public void IsOddOrEvenService_HasRemider_DividedByZeroException()
         {
             //Arrange
             var isOddOrEvenService = new IsOddOrEvenService();
-            //Act
-            var result = isOddOrEvenService.HasReminder(dividend, divisor);
             //Assert
-            Assert.Throws<DivideByZeroException>(() =>isOddOrEvenService.HasReminder(dividend, divisor));
+            Assert.Throws<DivideByZeroException>(() =>isOddOrEvenService.HasReminder(2, 0));
         }
     }
 }
